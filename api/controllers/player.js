522a31a02f515e335c047f687e5638befb3f35f3
ceulain <ceul1barth@hotmail.fr>
2016-07-17 15:14:59 +0200

    update getMatchPlayed function

--- api/controllers/player.js
+++ api/controllers/player.js
@@ -1,140 +1,153 @@
   //controller player
-'use strict'
+  'use strict'
 
-let realTime = require('../real_time');
-let getToken = require('./token');
-let jwt = require('jwt-simple');
-let config = require('../config/database');
-let Player = require('../models/player').modelPlayer;
-let Match = require('../models/match').modelMatch;
-let async = require('async');
+  let realTime = require('../real_time');
+  let getToken = require('./token');
+  let jwt = require('jwt-simple');
+  let config = require('../config/database');
+  let Player = require('../models/player').modelPlayer;
+  let Match = require('../models/match').modelMatch;
+  let Coach = require('../models/coach').modelCoach;
+  let async = require('async');
 
-exports.addPosition = function(req, res) {
+  exports.addPosition = function(req, res) {
 
-    let position = req.body.position;
-    let idPlayer = req.body.player_id;
-    let idMatch = req.body.match_id;
+      let position = req.body.position;
+      let idPlayer = req.body.player_id;
+      let idMatch = req.body.match_id;
 
-    let token = getToken(req.headers);
+      let token = getToken(req.headers);
 
-    if (token) {
-        let decoded = jwt.decode(token, config.secret);
-        let idCoach = decoded._id;
-
-
-        Player.findById(idPlayer, function(err, player) {
-            if (err)
-                res.status(404).json({
-                    error: err
-                });
-
-            console.log(player, idMatch, idCoach)
-            player.position = position;
-            realTime.updateStatistic_firebase(player, idMatch, idCoach, {
-                first_name: player.first_name,
-                last_name: player.last_name,
-                id: player._id,
-                favourite_position: player.favourite_position,
-                position: position
-            });
-            player.save();
-
-            res.status(201).json({
-                success: true,
-                player: player
-            });
-        })
-
-    } else {
-        return res.status(403).send({
-            success: false,
-            msg: 'No token provided.'
-        });
-    }
-};
-
-exports.getMatchPlayed = (req, res) => {
-
-    let token = getToken(req.headers);
-    let player_id = req.query.player_id;
-
-    if (token) {
-        let decoded = jwt.decode(token, config.secret);
-        let coach_id = decoded._id;
-        async.waterfall([
-            (cb) => {
-                Player.findById(player_id, (err, player) => {
-                    let matchPlayed = [];
-                    let statistics = player.statistics
-                    for (let stat of statistics) {
-                        matchPlayed.push(stat.match_id);
-                    }
-
-                    cb(null, matchPlayed);
-                })
-            },
-
-            (matchPlayed, cb) => {
-
-                Match.find({
-                    _id: {
-                        "$in": matchPlayed
-                    }, status: 'finished'
-                }, (err, match) => {
-                    if (err)
-                        throw err;
-
-                    cb(null, match);
-                });
-            },
-        ], (err, matchs) => {
-            if (err)
-                throw err;
-            res.status(202).json({
-                success: true,
-                matchs
-            });
-        });
-
-    } else {
-        return res.status(403).send({
-            success: false,
-            msg: 'No token provided.'
-        });
-    }
-};
+      if (token) {
+          let decoded = jwt.decode(token, config.secret);
+          let idCoach = decoded._id;
 
 
-exports.getStatisticsMatch = (req, res) => {
+          Player.findById(idPlayer, function(err, player) {
+              if (err)
+                  res.status(404).json({
+                      error: err
+                  });
 
-    let token = getToken(req.headers);
-    let player_id = req.query.player_id;
-    let match_id = req.query.match_id;
+              console.log(player, idMatch, idCoach)
+              player.position = position;
+              realTime.updateStatistic_firebase(player, idMatch, idCoach, {
+                  first_name: player.first_name,
+                  last_name: player.last_name,
+                  id: player._id,
+                  favourite_position: player.favourite_position,
+                  position: position
+              });
+              player.save();
 
-    if (token) {
+              res.status(201).json({
+                  success: true,
+                  player: player
+              });
+          })
 
-        Player.findById(player_id, (err, player) => {
+      } else {
+          return res.status(403).send({
+              success: false,
+              msg: 'No token provided.'
+          });
+      }
+  };
 
-            if (err)
-                throw err;
+  exports.getMatchPlayed = (req, res) => {
 
-            let statistics = player.statistics;
+      let token = getToken(req.headers);
+      let player_id = req.query.player_id;
 
-            for (let stat of statistics) {
-                if (stat.match_id = match_id) {
-                    return res.status(202).json({
-                        success: true,
-                        stat
-                    });
-                }
-            }
-        });
-    } else {
-        return res.status(403).send({
-            success: false,
-            msg: 'No token provided.'
-        });
-    }
+      if (token) {
+          let decoded = jwt.decode(token, config.secret);
+          let coach_id = decoded._id;
+
+          async.waterfall([
+
+              (cb) => {
+                console.log(coach_id);
+                  Coach.findById(coach_id, (err, coach) => {
+                      if (err)
+                          throw err;
+                      let matchPlayed = [];
+                      let matchs = coach.team.matchs;
+                      for (let match of matchs) {
+                          let playerSelected = match.playerSelected;
+                          if (playerSelected.indexOf(player_id) >= 0) {
+                              matchPlayed.push(match._id);
+                          }
+                      }
+
+                      cb(null, matchPlayed);
+                  });
+              },
+              (matchPlayed, cb) => {
+
+                  Match.find({
+                      _id: {
+                          "$in": matchPlayed
+                      },
+                      status: 'finished'
+                  }, (err, match) => {
+                      if (err)
+                          throw err;
+
+                      cb(null, match);
+                  });
+              },
+          ], (err, matchs) => {
+              if (err)
+                  throw err;
+              res.status(202).json({
+                  success: true,
+                  matchs
+              });
+          });
+
+      } else {
+          return res.status(403).send({
+              success: false,
+              msg: 'No token provided.'
+          });
+      }
+  };
 
 
-};
+  exports.getStatisticsMatch = (req, res) => {
+
+      let token = getToken(req.headers);
+      let player_id = req.query.player_id;
+      let match_id = req.query.match_id;
+
+      if (token) {
+
+          Player.findById(player_id, (err, player) => {
+
+              if (err)
+                  throw err;
+
+              let statistics = player.statistics;
+
+              for (let stat of statistics) {
+                  if (stat.match_id = match_id) {
+                      return res.status(202).json({
+                          success: true,
+                          stat
+                      });
+                  }
+              }
+          });
+      } else {
+          return res.status(403).send({
+              success: false,
+              msg: 'No token provided.'
+          });
+      }
+
+
+  };
+
+  exports.getGlobalStatistics = (req, res) => {
+
+  };
