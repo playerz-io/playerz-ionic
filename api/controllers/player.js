b1d4d2ef924f4058e4a16d9fa82ce70fc8d0e272
ceulain <ceul1barth@hotmail.fr>
2016-07-17 18:31:03 +0200

    add exports.getGlobalStatistics

--- api/controllers/player.js
+++ api/controllers/player.js
@@ -66,7 +66,6 @@
           async.waterfall([
 
               (cb) => {
-                console.log(coach_id);
                   Coach.findById(coach_id, (err, coach) => {
                       if (err)
                           throw err;
@@ -95,7 +94,7 @@
 
                       cb(null, match);
                   });
-              },
+              }
           ], (err, matchs) => {
               if (err)
                   throw err;
@@ -144,10 +143,104 @@
               msg: 'No token provided.'
           });
       }
-
-
   };
 
   exports.getGlobalStatistics = (req, res) => {
 
+      let token = getToken(req.headers);
+      let player_id = req.query.player_id;
+
+      let statisticsGlobal = {
+          "passesFailed": 0,
+          "crossesFailed": 0,
+          "saves": 0,
+          "claquettes": 0,
+          "sorties_aeriennes": 0,
+          "clean_sheet": 0,
+          "offSide": 0,
+          "relanceCompletion": 0,
+          "defensiveAction": 0,
+          "passesCompletion": 0,
+          "ballPlayed": 0,
+          "ballLost": 0,
+          "but": 0,
+          "substitute": 0,
+          "firstTeamPlayer": 0,
+          "matchPlayed": 0,
+          "beforeAssist": 0,
+          "attempts": 0,
+          "attemptsOffTarget": 0,
+          "attemptsOnTarget": 0,
+          "redCard": 0,
+          "yellowCard": 0,
+          "foulsCommitted": 0,
+          "foulsSuffered": 0,
+          "retrieveBalls": 0,
+          "assist": 0,
+      };
+
+      let keyStatisticsGlobal = Object.keys(statisticsGlobal);
+
+      if (token) {
+          let decoded = jwt.decode(token, config.secret);
+          let coach_id = decoded._id;
+
+          async.waterfall([
+
+              (cb) => {
+                  Coach.findById(coach_id, (err, coach) => {
+                      if (err)
+                          throw err;
+                      let matchPlayed = [];
+                      let matchs = coach.team.matchs;
+                      for (let match of matchs) {
+                          let playerSelected = match.playerSelected;
+                          if (playerSelected.indexOf(player_id) >= 0) {
+                              matchPlayed.push(match._id);
+
+                          }
+                      }
+
+                      cb(null, matchPlayed);
+                  });
+              },
+              (matchPlayed, cb) => {
+                  Player.findById(player_id, (err, player) => {
+
+                      let statistics = player.statistics;
+                      //convert array object in array string
+                      let matchPlayedString = matchPlayed.map(String);
+
+                      for (let stat of statistics) {
+
+                          if (matchPlayedString.indexOf(stat.match_id) >= 0) {
+
+                              for (let key of keyStatisticsGlobal) {
+                                  statisticsGlobal[key] += stat[key];
+                              }
+                          }
+                      }
+                      statisticsGlobal.passesCompletion = statisticsGlobal.passesCompletion / matchPlayed.length;
+                      statisticsGlobal.relanceCompletion = statisticsGlobal.passesCompletion / matchPlayed.length;
+                      
+                      cb(null, statisticsGlobal, matchPlayed);
+                  });
+
+              }
+          ], (err, matchs, matchPlayed) => {
+              if (err)
+                  throw err;
+              res.status(202).json({
+                  success: true,
+                  statisticsGlobal,
+                  nbrMatchPlayed: matchPlayed.length
+              });
+          });
+
+      } else {
+          return res.status(403).send({
+              success: false,
+              msg: 'No token provided.'
+          });
+      }
   };
