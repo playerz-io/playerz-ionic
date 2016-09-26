b7d693b8c5dbcf322fcba79c23d560ff22c59e75
ceulain <ceul1barth@hotmail.fr>
2016-09-26 22:04:50 +0200

    update getNameClub

--- api/controllers/clubs.js
+++ api/controllers/clubs.js
@@ -1,181 +1,33 @@
-exports.clubs = {
-    clubsFFF: [
-        // {
-        //     nomClub: '',
-        //     nbTeam:'',
-        //     ligue: '',
-        //     district: '',
-        //     correspondant: '',
-        //     adresse: '',
-        //     emailsClub: ['', ''],
-        //     telephones : [''],
-        //     fax: '',
-        //     siegeSocial: '',
-        //     stades: [
-        //         {
-        //             stadeNom: '',
-        //             stadeAdresse: '',
-        //             surface: '',
-        //         }
-        //     ],
-        //     president: '',
-        //     tresorier: '',
-        //     secretaire: '',
-        //     urlFFF: '',
-        // },
-        {
-            nomClub: 'RED STAR FOOTBALL CLUB',
-            nbTeam:'21',
-            ligue: 'LIGUE DE PARIS ILE DE FRANCE',
-            district: 'DISTRICT SEINE SAINT-DENIS',
-            correspondant: 'PILLON REGIS',
-            adresse: '92 RUE DU DOCTEUR BAUER 93400 ST OUEN',
-            emailsClub: ['secretariat@redstar.fr', 'redstarfc93@lpiff.fr'],
-            telephones : ['0140110426'],
-            fax: '01.40.12.48.17',
-            siegeSocial: '92, RUE DOCTEUR BAUER, - 93400 - ST OUEN',
-            stades: [
-                {
-                    Nom: '',
-                    stadeAdresse: '92,RUE DU DR.BAUER - 93400 - ST OUEN',
-                    surface: 'PVC',
-                }
-            ],
-            president: 'HADDAD PATRICE',
-            tresorier: 'GIBRE MARIA',
-            secretaire: 'VALLETTE ANTOINE',
-            urlFFF: 'http://www.fff.fr/la-vie-des-clubs/00154/infos-cles',
+'use strict';
 
-        },
-        {
-            nomClub: 'VITRY C.A.',
-            nbTeam:'21',
-            ligue: 'LIGUE DE PARIS ILE DE FRANCE',
-            district: 'DISTRICT VAL DE MARNE',
-            correspondant: 'FOPPIANI JOCELYNE',
-            adresse: '65 TER RUE DES PRES 94400 VITRY SUR SEINE',
-            emailsClub: ['cavitry@lpiff.fr'],
-            telephones : ['01 46 80 01 55', '01 46 80 64 98'],
-            fax: '01 46 80 01 55',
-            siegeSocial: '92, RUE DOCTEUR BAUER 93400 ST OUEN',
-            stades: [
-                {
-                    stadeNom: 'STADE PILLAUDIN',
-                    stadeAdresse: 'AV. DE LA REPUBLIQUE 94400 VITRY SUR SEINE',
-                    surface: 'PVC',
-                },
-                {
-                    stadeNom: '',
-                    stadeAdresse: '94 RUE GABRIEL PERI 94400 VITRY SUR SEINE',
-                    surface: 'Pelouse naturelle',
-                }
-            ],
-            president: 'FOPPIANI JEAN JACQUES',
-            tresorier: 'BLANCHARDIE JEAN MARC',
-            secretaire: 'ERGIL FUNDA',
-            urlFFF: 'http://www.fff.fr/la-vie-des-clubs/00156/infos-cles',
-        },
-        {
-            nomClub: 'CLICHY S/SEINE U.S.',
-            nbTeam:'18',
-            ligue: 'LIGUE DE PARIS ILE DE FRANCE',
-            district: 'DISTRICT DES HAUTS-DE-SEINE',
-            correspondant: 'BAZZONI JEAN LOUIS',
-            adresse: '53 RUE KLOCK 92110 CLICHY',
-            emailsClub: ['usa-clichy@lpiff.fr', 'clichysports@lpiff.fr'],
-            telephones : ['01 47 56 15 62', '06 89 33 13 05'],
-            fax: '',
-            siegeSocial: '2 RUE JEAN WALTER 92110 CLICHY SUR SEINE',
-            stades: [
-                {
-                    stadeNom: '',
-                    stadeAdresse: '55 RUE VILLENEUVE 92110 CLICHY',
-                    surface: 'Gazon synthétique type SYE',
-                }
-            ],
-            president: 'GODON FRANCIS',
-            tresorier: 'BAZZONI JEAN LOUIS',
-            secretaire: 'ADREF OUSSAMA',
-            urlFFF: 'http://www.fff.fr/la-vie-des-clubs/00157/infos-cles',
-        },
-        {
-            nomClub: 'GARENNE COLOMBES A.F.',
-            nbTeam:'17',
-            ligue: 'LIGUE DE PARIS ILE DE FRANCE',
-            district: 'DISTRICT DES HAUTS-DE-SEINE',
-            correspondant: 'DALLE EVELYNE',
-            adresse: 'MAISON DES CLUBS, 22 Rue Estienne d Orves 92250 LA GARENNE COLOMBES',
-            emailsClub: ['afgc92@wanadoo.fr', 'afgc@lpiff.fr' ],
-            telephones : ['09 77 56 87 40'],
-            fax: '01 56 83 21 68',
-            siegeSocial: 'MAISON DES CLUBS, 22 Rue Estienne d Orves 92250 LA GARENNE COLOMBES',
-            stades: [
-                {
-                    stadeNom: '',
-                    stadeAdresse: '36, Rue Paul Prouteau 92250 LA GARENNE COLOMBES',
-                    surface: 'Gazon synthétique type SY',
-                }
-            ],
-            president: 'VIAUD ALAIN',
-            tresorier: 'KETTOU MERIEME',
-            secretaire: 'JOAQUIM GOMES PATRICIA',
-            urlFFF: 'http://www.fff.fr/la-vie-des-clubs/00161/infos-cles',
-        },
-        {
-            nomClub: 'CHARENTON C.A.P.',
-            nbTeam:'20',
-            ligue: 'LIGUE DE PARIS ILE DE FRANCE',
-            district: 'DISTRICT VAL DE MARNE',
-            correspondant: 'GOURDOU JEAN PHILIPPE',
-            adresse: 'STADE HENRI GUERIN ILE MARTINET 94220 CHARENTON LE PONT',
-            emailsClub: ['capcharenton@lpiff.fr', 'jpgourdou@capcharenton.fr'],
-            telephones : ['07.60.61.30.20'],
-            fax: '',
-            siegeSocial: 'STADE HENRI GUERIN, ILE MARTINET, - 94220 - CHARENTON',
-            stades: [
-                {
-                    stadeNom: 'STADE HENRI GUERIN',
-                    stadeAdresse: 'QUAI DE CHARENTON ILE MARTINET - 94220 - CHARENTON LE PONT',
-                    surface: 'Gazon synthétique type SYE',
-                }
-            ],
-            president: 'COUSO CARLOS',
-            tresorier: 'GUESSANT BERTRAND',
-            secretaire: 'GODIN VERONIQUE',
-            urlFFF: 'http://www.fff.fr/la-vie-des-clubs/00164/infos-cles',
-        },
-        {
-            nomClub: 'JEANNE D ARC ST SERVAN',
-            nbTeam:'10',
-            ligue: 'LIGUE BRETAGNE DE FOOTBALL',
-            district: 'DISTRICT ILLE ET VILAINE',
-            correspondant: 'VIOT YANNICK',
-            adresse: '6 RUE STE BARBE 35400 ST MALO',
-            emailsClub: ['jasaintservan@yahoo.fr', '4500014@footlbf.fr'],
-            telephones : ['02 99 40 32 12'],
-            fax: '',
-            siegeSocial: '22 Avenue de Marville, Stade de Marville, - 35400 - ST MALO',
-            stades: [
-                {
-                    stadeNom: '',
-                    stadeAdresse: 'RUE HENRI LEMARIE DUGUAY TROUIN - 35400 - ST MALO',
-                    surface: 'Gazon synthétique type SYE',
-                }
-            ],
-            president: 'PLIHON JEAN LUC',
-            tresorier: '',
-            secretaire: 'VIOT YANNICK',
-            urlFFF: 'http://www.fff.fr/la-vie-des-clubs/00165/infos-cles',
-        }
-    ]
-};
+let alsace = require('../sports/football/fff/ligueAlsace.json');
+let aquitaine = require('../sports/football/fff/ligueAquitaine.json');
+let atlantique = require('../sports/football/fff/ligueAtlantique.json');
+let auvergne = require('../sports/football/fff/ligueAuvergne.json');
+let basse_normandie = require('../sports/football/fff/ligueBasseNormandie.json');
+let bourgogne = require('../sports/football/fff/ligueBourgogne.json');
+let bretagne = require('../sports/football/fff/ligueBretagne.json');
+let centre_ouest = require('../sports/football/fff/ligueCentreOuest.json');
+let centre_val_de_loire = require('../sports/football/fff/ligueCentreValDeLoire.json');
+let champagne_ardennne = require('../sports/football/fff/ligueChampagneArdenne.json');
+let corse = require('../sports/football/fff/ligueCorse.json');
+let maine = require('../sports/football/fff/ligueDuMaine.json');
+let languedoc_roussillon = require('../sports/football/fff/ligueLanguedocRoussillon.json');
+let lorraine = require('../sports/football/fff/ligueLorraine.json');
+let mediterreanee = require('../sports/football/fff/ligueMediterreanée.json');
+let nord_pas_de_calais = require('../sports/football/fff/ligueNordPasDeCalais.json');
+let normandie = require('../sports/football/fff/ligueNormandie.json');
+let ile_de_france = require('../sports/football/fff/ligueParisIleDeFrance.json');
+let picardie = require('../sports/football/fff/liguePicardie.json');
+let rhone_alpes = require('../sports/football/fff/ligueRhoneAlpes.json');
+let val_de_loire = require('../sports/football/fff/ligueValDeLoire.json');
+
+
+let club =  [].concat(alsace, aquitaine, atlantique, auvergne, basse_normandie, bourgogne, bretagne, centre_ouest, centre_val_de_loire, champagne_ardennne, corse, maine, languedoc_roussillon, lorraine, mediterreanee, nord_pas_de_calais, normandie, ile_de_france, picardie, rhone_alpes, val_de_loire);
 
 exports.getNameClub = (req, res) => {
-    "use strict";
 
-    let clubs = exports.clubs.clubsFFF;
-
-    let arrayNameClub = clubs.map((club) => {
+    let arrayNameClub = club.map((club) => {
         return club.nomClub;
     });
 
@@ -183,27 +35,3 @@ exports.getNameClub = (req, res) => {
         arrayNameClub
     });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
